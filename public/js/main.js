1a2c4ee480e9f8115b43fec4499bf748024fedd0
SamPerry93 <stperry67@gmail.com>
2021-08-11 20:10:46 +0300

    added rooms and users

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 const chatForm = document.getElementById("chat-form")
 const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
+//getuser and rooom
+const {username, room} = Qs.parse(location.search,{
+    ignoreQueryPrefix: true
+})
+
 const socket = io();
+
+//join room
+socket.emit('joinRoom', {username, room})
+
+//get room and users
+socket.on('roomUsers', ({room,users}) => {
+    outputRoomName(room);
+    outputUsers(users);
+})
 
 socket.on('message', message=>{
     console.log(message)
@@ -8,7 +24,7 @@ socket.on('message', message=>{
     //scroll down to new message
     chatMessages.scrollTop = chatMessages.scrollHeight
 })
-
+console.log(username,room)
 //message submit 
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -32,4 +48,16 @@ function outputMessage(message){
         </p>
     `
     document.querySelector('.chat-messages').appendChild(div)
+}
+
+//add room name to DOM
+function outputRoomName(room){
+    roomName.innerText = room;
+
+}
+//add users to DOM
+function outputUsers(users){
+    userList.innerHTML = `
+        ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `
 }
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
 const formatMessage = require('./utils/messages')
+const {userJoin, getCurrentUser,userLeave,getRoomUsers} = require('./utils/users')
 //set static files
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -14,26 +15,41 @@ const ultimateUser = 'ultimate_usr'
 //run when client connects
 io.on('connection', socket => {
     console.log('new websocket connection')
+    //joine room
+    socket.on('joinRoom', ({username,room})=>{
+        const user = userJoin(socket.id, username,room)
+        socket.join(user.room)
 
-    //to just the individual
-    socket.emit('message', formatMessage(ultimateUser,'Welcome to HackChat'));
+            //to just the individual
+        socket.emit('message', formatMessage(ultimateUser,'Welcome to HackChat'));
 
-        //to everyone other than the individual 
-    //broadcast when user connects
-    socket.broadcast.emit('message', formatMessage(ultimateUser,'a user has joined chat'))
+            //to everyone other than the individual 
+        //broadcast when user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(ultimateUser,`${user.username} has joined room`))
 
+        //send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users:getRoomUsers(user.room)
+        })
+    })
         //to everybody////////
     // io.emit()
         /////////////////////
 
     //runs on disconnect
     socket.on('disconnect', ()=>{
-        io.emit('message', formatMessage(ultimateUser,'A user has left the chat'));
+        const user = userLeave(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message', formatMessage(ultimateUser,`${user.username} has left the room`));
+        }
     })
 
     //listen for messages
     socket.on('chatMessage', (msg) => {
-        io.emit('message',formatMessage('anon', msg))
+        const user = getCurrentUser(socket.id)
+        io.to(user.room).emit('message',formatMessage(user.username, msg))
     })
 })  
 
